17bf01bff437a6514f0f2c0e721e2002ad9a3a58
Lebranc Convas <lebrancconvas@gmail.com>
2017-12-01 03:59:18 +0700

    Input Number and Print in Thai Language

---
+++ next.py
@@ -0,0 +1,21 @@
+money = input("Insert your money : ");
+if int(money) == 1000:
+    print("หนึ่งพัน");
+elif int(money) == 2000:
+    print("สองพัน");
+elif int(money) == 3000:
+    print("สามพัน");
+elif int(money) == 4000:
+    print("สี่พัน");
+elif int(money) == 5000:
+    print("ห้าพัน");
+elif int(money) == 6000:
+    print("หกพัน");
+elif int(money) == 7000:
+    print("เจ็ดพัน");
+elif int(money) == 8000:
+    print("แปดพัน");
+elif int(money) == 9000:
+    print("เก้าพัน");
+else:
+    print(int(money));
