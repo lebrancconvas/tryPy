1bc0b72192ddd5ee51496d70a5425ce6b04d0cf7
Lebranc Convas <lebrancconvas@gmail.com>
2017-12-01 14:00:29 +0700

    Update Feature
    We add the feature that user can select that what they want to do? instead of input THB and Output all of three currency (USD , Yen ,Yuan)

--- CurrencyChange.py
+++ CurrencyChange.py
@@ -1,8 +1,25 @@
-baht = input("Insert your money(THB) : ");
-bahtCurrency = float(baht);
-usdollarCurrency = bahtCurrency * 0.031;
-yenCurrency = bahtCurrency * 3.45;
-yuanCurrency = bahtCurrency * 0.2;
-print(str(usdollarCurrency) + " US Dollar");
-print(str(yenCurrency) + " Yen");
-print(str(yuanCurrency) + " Yuan");
+print("What do you want to do?");
+print("Want to transform currency from THB to US Dollar? Press 'a' ");
+print("Want to transform currency from THB to Yen? Press 'b' ");
+print("Want to transform currency from THB to Yuan? Press 'c'");
+trans = input("What do you want to do? >>> ");
+if trans == "a":
+    baht = input("Insert your money(THB) : ");
+    bahtCurrency = float(baht);
+    """1 BAHT = 0.0031 US DOLLAR"""
+    usdollarCurrency = bahtCurrency * 0.031;
+    print(str(usdollarCurrency) + " US Dollar");
+elif trans == "b":
+    baht = input("Insert your money(THB) : ");
+    bahtCurrency = float(baht);
+    """1 BAHT = 3.45 YEN"""
+    yenCurrency = bahtCurrency * 3.45;
+    print(str(yenCurrency) + " Yen");
+elif trans == "c":
+     baht = input("Insert your money(THB) : ");
+     bahtCurrency = float(baht);
+     """1 BAHT = 0.2 YUAN"""
+     yuanCurrency = bahtCurrency * 0.2;
+     print(str(yuanCurrency) + " Yuan");
+else:
+    print("We don't know what you want to do, Bye.");
